e055c5856022ad994ef6287de23ffaab403ee02b
Shaun <shaunsibu@gmail.com>
2014-09-11 22:17:35 -0600

    CM Theme Engine for Frameworks_base-May be incomplete
    Change-Id: I3634152d9588660428aee25ddf83097559bf6063

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -576,14 +576,6 @@ public abstract class PackageManager {
     public static final int INSTALL_FAILED_VERSION_DOWNGRADE = -25;
 
     /**
-     * Installation return code: this is passed to the {@link IPackageInstallObserver} by
-     * {@link #installPackage(android.net.Uri, IPackageInstallObserver, int)} if
-     * the package is from unknown sources but not trusted sources.
-     * @hide
-     */
-    public static final int INSTALL_FAILED_UNKNOWN_SOURCES = -26;
-
-    /**
      * Installation parse return code: this is passed to the {@link IPackageInstallObserver} by
      * {@link #installPackage(android.net.Uri, IPackageInstallObserver, int)}
      * if the parser was given a path that is not a file, or does not end with the expected
@@ -682,6 +674,35 @@ public abstract class PackageManager {
      * @hide
      */
     public static final int INSTALL_FAILED_USER_RESTRICTED = -111;
+
+    /**
+     * Used by themes
+     * Installation failed return code: this is passed to the {@link IPackageInstallObserver} by
+     * {@link #installPackage(android.net.Uri, IPackageInstallObserver, int)}
+     * if the system failed to install the theme because aapt could not compile the app
+     * @hide
+     */
+    public static final int INSTALL_FAILED_THEME_AAPT_ERROR = -400;
+
+    /**
+     * Used by themes
+     * Installation failed return code: this is passed to the {@link IPackageInstallObserver} by
+     * {@link #installPackage(android.net.Uri, IPackageInstallObserver, int)}
+     * if the system failed to install the theme because idmap failed
+     * apps.
+     * @hide
+     */
+    public static final int INSTALL_FAILED_THEME_IDMAP_ERROR = -401;
+
+    /**
+     * Used by themes
+     * Installation failed return code: this is passed to the {@link IPackageInstallObserver} by
+     * {@link #installPackage(android.net.Uri, IPackageInstallObserver, int)}
+     * if the system failed to install the theme for an unknown reason
+     * apps.
+     * @hide
+     */
+    public static final int INSTALL_FAILED_THEME_UNKNOWN_ERROR = -402;
 
     /**
      * Flag parameter for {@link #deletePackage} to indicate that you don't want to delete the
@@ -1760,17 +1781,6 @@ public abstract class PackageManager {
     public abstract List<PackageInfo> getInstalledPackages(int flags, int userId);
 
     /**
-     * Return a List of all theme packages that are installed
-     * on the device.
-     *
-     * @return A List of PackageInfo objects, one for each theme package
-     *         that is installed on the device.
-     *
-     * @hide
-     */
-    public abstract List<PackageInfo> getInstalledThemePackages();
-
-    /**
      * Check whether a particular package has been granted a particular
      * permission.
      *
@@ -2618,6 +2628,18 @@ public abstract class PackageManager {
     public abstract Resources getResourcesForApplicationAsUser(String appPackageName, int userId)
             throws NameNotFoundException;
 
+    /** @hide */
+    public abstract Resources getThemedResourcesForApplication(ApplicationInfo app,
+            String themePkgName) throws NameNotFoundException;
+
+    /** @hide */
+    public abstract Resources getThemedResourcesForApplication(String appPackageName,
+            String themePkgName) throws NameNotFoundException;
+
+    /** @hide */
+    public abstract Resources getThemedResourcesForApplicationAsUser(String appPackageName,
+            String themePkgName, int userId) throws NameNotFoundException;
+
     /**
      * Retrieve overall information about an application package defined
      * in a package archive file
@@ -3276,4 +3298,10 @@ public abstract class PackageManager {
      * @hide
      */
     public abstract void setComponentProtectedSetting(ComponentName componentName, boolean newState);
+
+    /**
+     * Updates the theme icon res id for the new theme
+     * @hide
+     */
+    public abstract void updateIconMaps(String pkgName);
 }
--- test-runner/src/android/test/mock/MockPackageManager.java
+++ test-runner/src/android/test/mock/MockPackageManager.java
@@ -406,6 +406,27 @@ public class MockPackageManager extends PackageManager {
         throw new UnsupportedOperationException();
     }
 
+    /** @hide */
+    @Override
+    public Resources getThemedResourcesForApplication(ApplicationInfo app, String themePkgName)
+            throws NameNotFoundException {
+        return null;
+    }
+
+    /** @hide */
+    @Override
+    public Resources getThemedResourcesForApplication(String appPackageName, String themePkgName)
+            throws NameNotFoundException {
+        return null;
+    }
+
+    /** @hide */
+    @Override
+    public Resources getThemedResourcesForApplicationAsUser(String appPackageName,
+            String themePkgName, int userId) throws NameNotFoundException {
+        return null;
+    }
+
     @Override
     public PackageInfo getPackageArchiveInfo(String archiveFilePath, int flags) {
         throw new UnsupportedOperationException();
@@ -640,14 +661,6 @@ public class MockPackageManager extends PackageManager {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * @hide - to match hiding in superclass
-     */
-    @Override
-    public List<PackageInfo> getInstalledThemePackages() {
-        throw new UnsupportedOperationException();
-    }
-
     @Override
     public void verifyPendingInstall(int id, int verificationCode) {
         throw new UnsupportedOperationException();
@@ -672,6 +685,9 @@ public class MockPackageManager extends PackageManager {
      */
     @Override
     public void setComponentProtectedSetting(ComponentName componentName, boolean newState) {
+    }
+
+    public void updateIconMaps(String pkgName) {
         throw new UnsupportedOperationException();
     }
 }
