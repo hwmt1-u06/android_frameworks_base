f763d801dd1e2d7295010104f5abdb8d05e6f2fc
Shaun <shaunsibu@gmail.com>
2014-09-20 01:24:29 -0600

    Revert "Add Soft Reboot Option."
    This reverts commit 6b4322c656d73db3797d91bfcfa52eabf50f519c.

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -22,11 +22,9 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
 
-import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.IActivityManager;
 import android.app.Profile;
 import android.app.ProfileManager;
 import android.content.BroadcastReceiver;
@@ -100,8 +98,6 @@ import com.android.internal.util.beanstalk.NamelessActions;
 class GlobalActions implements DialogInterface.OnDismissListener, DialogInterface.OnClickListener  {
 
     private static final String TAG = "GlobalActions";
-
-    private static final String SOFT_REBOOT = "soft_reboot";
 
     private static final boolean SHOW_SILENT_TOGGLE = true;
 
@@ -1568,20 +1564,4 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             return super.onKeyUp(keyCode, event);
         }
     }
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (rebootReasons[rebootIndex].equals(SOFT_REBOOT)) {
-                            try {
-                                final IActivityManager am = ActivityManagerNative.asInterface(
-                                        ServiceManager.checkService("activity"));
-                                if (am != null) {
-                                    am.restart();
-                                }
-                            } catch (RemoteException e) {
-                                Log.e(TAG, "failure trying to perform soft reboot", e);
-                            }
-                        } else {
-                            mWindowManagerFuncs.reboot(rebootReasons[rebootIndex]);
-                        }
-                    }
 }
