1e11b881e04d8b6b48b56291121841526a71814c
Shaun <shaunsibu@gmail.com>
2014-10-07 10:23:54 -0600

    [1/2] Feature: Allow OmniSwitch to be set as default recents.
    Change-Id: I17fdedb9dfaf24370a52ba181064f7660a9a0db1

--- packages/SystemUI/src/com/android/systemui/recent/Recents.java
+++ packages/SystemUI/src/com/android/systemui/recent/Recents.java
@@ -53,7 +53,8 @@ public class Recents extends SystemUI implements RecentsComponent {
     }
 
     @Override
-    public void toggleRecents(Display display, int layoutDirection, View statusBarView) {
+    public void toggleRecents(Display display, int layoutDirection,
+            View statusBarView, int expandedDesktopStyle) {
         if (DEBUG) Log.d(TAG, "toggle recents panel");
         try {
             TaskDescription firstTask = RecentTasksLoader.getInstance(mContext).getFirstTask();
@@ -138,7 +139,11 @@ public class Recents extends SystemUI implements RecentsComponent {
 
 
                 DisplayMetrics dm = new DisplayMetrics();
-                display.getMetrics(dm);
+                if (expandedDesktopStyle == 2) {
+                    display.getRealMetrics(dm);
+                } else {
+                    display.getMetrics(dm);
+                }
                 // calculate it here, but consider moving it elsewhere
                 // first, determine which orientation you're in.
                 final Configuration config = res.getConfiguration();
