18abb509b40c3cd019e6a40895a2960f8cbbe1cd
Shaun <shaunsibu@gmail.com>
2014-10-11 11:03:36 -0600

    Need this is Settings provider for Wifi Auto COnntect Commit.
    Change-Id: I5db72043a4cb3570c3611a50fa06f4708f5af48c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5898,6 +5898,25 @@ public final class Settings {
          * @hide
          */
         public static final String QUICK_SETTINGS_RIBBON_TILES = "quick_settings_ribbon_tiles";
+		
+        /**
+         * Whether wifi settings will connect to access point automatically
+         * 0 = automatically
+         * 1 = manually
+         * @hide
+         */
+        public static final String WIFI_AUTO_CONNECT_TYPE = "wifi_auto_connect_type";
+
+        /**
+         * Whether wifi settings will connect to access point automatically when
+         * network from mobile network transform to Wifi network
+         * 0 = automatically
+         * 1 = manually
+         * 2 = always ask
+         *
+         * @hide
+         */
+        public static final String DATA_TO_WIFI_CONNECT_TYPE = "data_to_wifi_connect_type";		
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
