ec384f7bf6d56521a385f4e46e493d927b7ad82a
Shaun <shaunsibu@gmail.com>
2014-10-22 15:23:38 -0600

    Revert "BASE: Feature Add: Recents clear all app lock option"
    This reverts commit 7670a2f14b5a6db85dd73e627fee18e014f91631.

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -92,7 +92,6 @@ public class RecentsActivity extends Activity {
                 R.anim.recents_return_to_launcher_enter,
                 R.anim.recents_return_to_launcher_exit);
         mForeground = false;
-        mRecentsPanel.saveLockedTasks();
         super.onPause();
     }
 
