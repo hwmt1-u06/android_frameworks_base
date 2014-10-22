5e5ca523c65959da668a77cfc454b2c9f12cbf31
Shaun <shaunsibu@gmail.com>
2014-10-22 15:40:34 -0600

    Revert "Show more info at start up of app being worked on."
    This reverts commit 0ca998254efd297c08b776546960d86c8a6a2e5c.

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -42,7 +42,6 @@ import com.android.internal.app.IMediaContainerService;
 import com.android.internal.app.ResolverActivity;
 import com.android.internal.content.NativeLibraryHelper;
 import com.android.internal.content.PackageHelper;
-import com.android.internal.policy.impl.PhoneWindowManager;
 import com.android.internal.util.FastPrintWriter;
 import com.android.internal.util.FastXmlSerializer;
 import com.android.internal.util.XmlUtils;
@@ -144,7 +143,6 @@ import android.util.SparseArray;
 import android.util.Xml;
 import android.view.Display;
 import android.view.WindowManager;
-import android.view.WindowManagerPolicy;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
@@ -614,9 +612,6 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     // Stores a list of users whose package restrictions file needs to be updated
     private HashSet<Integer> mDirtyUsers = new HashSet<Integer>();
-
-    WindowManager mWindowManager;
-    private final WindowManagerPolicy mPolicy; // to set packageName
 
     final private DefaultContainerConnection mDefContainerConn =
             new DefaultContainerConnection();
@@ -1218,9 +1213,8 @@ public class PackageManagerService extends IPackageManager.Stub {
 
         mInstaller = installer;
 
-        mWindowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
-        Display d = mWindowManager.getDefaultDisplay();
-        mPolicy = new PhoneWindowManager();
+        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        Display d = wm.getDefaultDisplay();
         d.getMetrics(mMetrics);
 
         File frameworkDir;
@@ -4113,14 +4107,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                             if (!isFirstBoot()) {
                                 i.getAndIncrement();
                                 try {
-                                    // give the packagename to the PhoneWindowManager
-                                    ApplicationInfo ai;
-                                    try {
-                                        ai = mContext.getPackageManager().getApplicationInfo(p.packageName, 0);
-                                    } catch (Exception e) {
-                                        ai = null;
-                                    }
-                                    mPolicy.setPackageName((String) (ai != null ? mContext.getPackageManager().getApplicationLabel(ai) : p.packageName));
                                     ActivityManagerNative.getDefault().showBootMessage(
                                         mContext.getResources().getString(
                                             com.android.internal.R.string.android_upgrading_apk,
