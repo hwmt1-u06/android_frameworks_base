cbfc3721c019a049ff362669490edef7cc36df8f
Shaun <shaunsibu@gmail.com>
2014-10-23 12:34:03 -0600

    Converting to new TRDS 3.0
    Change-Id: Ic29cab1abf62c4d00cc33ae12e9ada1bf1b47bc7

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -414,6 +414,7 @@ public class ResourcesManager {
                 DisplayMetrics dm = defaultDisplayMetrics;
                 final boolean hasOverrideConfiguration = key.hasOverrideConfiguration();
                 boolean themeChanged = (changes & ActivityInfo.CONFIG_THEME_RESOURCE) != 0;
+                final boolean themeChanged2 = (changes & ActivityInfo.CONFIG_UI_THEME_MODE) != 0;
                 if (themeChanged) {
                     AssetManager am = r.getAssets();
                     if (am.hasThemeSupport()) {
@@ -430,9 +431,6 @@ public class ResourcesManager {
                     }
                 }
                 if (!isDefaultDisplay || hasOverrideConfiguration) {
-                final boolean themeChanged = (changes & ActivityInfo.CONFIG_UI_THEME_MODE) != 0;
-                if ((!isDefaultDisplay || hasOverrideConfiguration)) {
-
                     if (tmpConfig == null) {
                         tmpConfig = new Configuration();
                     }
@@ -448,7 +446,7 @@ public class ResourcesManager {
                 } else {
                     r.updateConfiguration(config, dm, compat);
                 }
-                if (themeChanged) {
+                if (themeChanged || themeChanged2) {
                     r.updateStringCache();
                 }
                 //Slog.i(TAG, "Updated app resources " + v.getKey()
