7f044d9e233fd5d6830560ccdd69312c3b012e7c
Shaun <shaunsibu@gmail.com>
2014-10-30 22:41:22 -0600

    Revert "Fix glitch appearing when using translucent system bars in landscape."
    This reverts commit 62cca1816b3406b8f3fa67d49fc59f5ef7dd25d4.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
@@ -45,8 +45,6 @@ public final class NavigationBarTransitions extends BarTransitions {
     private final NavigationBarView mView;
     private final IStatusBarService mBarService;
 
-    private View mStatusBarBlocker;
-
     private boolean mLightsOut;
     private boolean mVertical;
     private int mRequestedMode;
@@ -60,7 +58,6 @@ public final class NavigationBarTransitions extends BarTransitions {
     }
 
     public void init(boolean isVertical) {
-        mStatusBarBlocker = mView.findViewById(R.id.status_bar_blocker);
         setVertical(isVertical);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/, true /*force*/);
@@ -120,9 +117,6 @@ public final class NavigationBarTransitions extends BarTransitions {
 
         // apply to lights out
         applyLightsOut(mode == MODE_LIGHTS_OUT, animate, force);
-
-        final boolean isTranslucent = mode != MODE_OPAQUE && mode != MODE_LIGHTS_OUT;
-        fadeContent(mStatusBarBlocker, isTranslucent ? 1f : 0f);
     }
 
     private float alphaForMode(int mode) {
